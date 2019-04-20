5734c7795ebdb6ae79d8b20792f60fef4c290330
Nekobitlz <jokers.bit@gmail.com>
2019-04-20 14:59:06 +0300

    Removed objects from enum and now he describes only enemies

---
+++ app/src/main/java/com/nekobitlz/meteorite_attack/enums/EnemyType.java
@@ -0,0 +1,5 @@
+package com.nekobitlz.meteorite_attack.enums;
+
+public enum EnemyType {
+    EnemyShip, Meteorite, BorderDestroyer, Exploder
+}
--- app/src/main/java/com/nekobitlz/meteorite_attack/enums/Objects.java
+++
@@ -1,5 +0,0 @@
-package com.nekobitlz.meteorite_attack.enums;
-
-public enum Objects {
-    EnemyShip, Star, Laser, Meteorite, BorderDestroyer, Exploder
-}
